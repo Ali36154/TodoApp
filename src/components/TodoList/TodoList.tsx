de3cb6ccbb8d1493afc1665f9fc6189985a2b2f9
Ali Raza <hafizaliraza247@gmail.com>
2024-07-20 10:51:10 +0500

    Edit Todo Function

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from '../../model';
 import { MdDelete } from "react-icons/md";
 import { FaUndo } from "react-icons/fa";
 import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import { CiEdit } from "react-icons/ci";
+import { IoSaveOutline } from "react-icons/io5";
+
 interface Props {
   toDos: Todo[];
   setToDos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 export const TodoList: React.FC<Props> = ({ toDos, setToDos }) => {
+  const [editId, setEditId] = useState<number | null>(null);
+  const [newText, setNewText] = useState<string>("");
+
   const handleToggleDone = (id: number) => {
     setToDos(toDos.map(todo => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo));
   };
@@ -17,28 +23,59 @@ export const TodoList: React.FC<Props> = ({ toDos, setToDos }) => {
     setToDos(toDos.filter(todo => todo.id !== id));
   };
 
+  const handleEdit = (id: number, text: string) => {
+    setEditId(id);
+    setNewText(text);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewText(e.target.value);
+  };
+
+  const handleSave = (id: number) => {
+    setToDos(toDos.map(todo => todo.id === id ? { ...todo, todo: newText } : todo));
+    setEditId(null);
+    setNewText("");
+  };
+
   return (
     <div className="todo-list w-full max-w-screen-sm p-5">
       {toDos.map(todo => (
-        <div key={todo.id} style={{
-            backgroundColor: todo.isDone ? '#FFD700' : '	#FF7F7F'
-          }} className="todo-item rounded-lg m-3 p-3 flex  justify-between">
-          <span
-            className="px-2"
-            style={{
-              textDecoration: todo.isDone ? 'line-through' : 'none',
-              
-            }}
-          >
-            {todo.todo}
-          </span>
+        <div
+          key={todo.id}
+          style={{
+            backgroundColor: todo.isDone ? '#FFD700' : '#FF7F7F'
+          }}
+          className="todo-item rounded-lg m-3 p-3 flex justify-between"
+        >
+          {editId === todo.id ? (
+            <input
+              className="px-2 outline-none bg-transparent"
+              type="text"
+              value={newText}
+              onChange={handleChange}
+            />
+          ) : (
+            <span
+              className="px-2"
+              style={{
+                textDecoration: todo.isDone ? 'line-through' : 'none',
+              }}
+            >
+              {todo.todo}
+            </span>
+          )}
           <div className='w-1/6 flex justify-around'>
-          <button onClick={() => handleToggleDone(todo.id)}>
-            {todo.isDone ? <span><FaUndo /></span> : <IoCheckmarkDoneSharp />}
-          </button>
-          <button onClick={() => handleDelete(todo.id)}><MdDelete /></button>
+            <button onClick={() => handleToggleDone(todo.id)}>
+              {todo.isDone ? <FaUndo /> : <IoCheckmarkDoneSharp />}
+            </button>
+            <button onClick={() => handleDelete(todo.id)}><MdDelete /></button>
+            {editId === todo.id ? (
+              <button onClick={() => handleSave(todo.id)}><IoSaveOutline /></button>
+            ) : (
+              <button onClick={() => handleEdit(todo.id, todo.todo)}><CiEdit /></button>
+            )}
           </div>
-          
         </div>
       ))}
     </div>
